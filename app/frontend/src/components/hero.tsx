cd909ef458574152110be8cfcdd6c79d42b0bce8
atlance23 <caelantandrews@outlook.com>
2025-09-24 11:45:25 -0400

    Add Hero Component

--- app/frontend/src/components/hero.tsx
+++ app/frontend/src/components/hero.tsx
@@ -1,7 +1,24 @@
 function Hero() {
     return (
         <>
-            // TO-DO: Add hero section
+            <section className="heroSection">
+                <div className="heroContentContainer">
+                    <h1 className="seoHeading">
+                        Full-Stack Software Development With Go, C#, and React
+                    </h1>
+                    <h5 className="displayHeading">
+                        Caldwell & Co.
+                    </h5>
+                    <div className="ctaContainer">
+                        <div className="btnPrimary">
+
+                        </div>
+                        <div className="btnSecondary">
+
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
